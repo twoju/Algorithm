3868fe0a65db3aaafef891fadf7ecd8a30c60fad
조원주 <dnjswn9178@gmail.com>
2023-06-14 16:00:41 +0900

    solve: 달리기 경주

--- python3/달리기경주.py
+++ python3/달리기경주.py
@@ -1,37 +1,51 @@
+# Input
 players = ["mumu", "soe", "poe", "kai", "mine"]
 callings = ["kai", "kai", "mine", "mine"]
 
-    # i = 0
-    # while i < call:
-    #     idx = players.index(callings[i])
-    #     players[idx], players[idx-1] = players[idx-1], players[idx]
-    #     i += 1
-
+# 1번 풀이
+# while 문을 이용해서 이름 불린 순서에 따라 선수의 index 값을 찾아 앞 선수와 순서를 바꿔준다.
 
 # def solution(players, callings):
-#     for i in range(len(callings)):
+#     call = len(callings)
+#     i = 0
+#     while i < call:
 #         idx = players.index(callings[i])
-#         players[idx], players[idx - 1] = players[idx - 1], players[idx]
+#         players[idx], players[idx-1] = players[idx-1], players[idx]
+#         i += 1
 #     answer = players
 #     return answer
 
 
+# 2번 풀이
+# for 문을 활용해서 순서와 이름을 담은 딕셔너리를 생성한 후, 이름 불린 선수와 앞선수를 바꿔준다.
+
+# def solution(players, callings):
+#
+#     for call in callings:
+#         player = {string: i for i, string in enumerate(players)}
+#         pre, index = player[call] - 1, player[call]
+#         players[pre], players[index] = players[index], players[pre]
+#
+#     answer = players
+#     return answer
+
+
+# 3번 풀이
+# for 문을 활용하고, .index() 를 사용해서 앞 선수와 순서를 바꿔준다.
+# 실행 시간이 가장 짧고, 가장 많은 테스트 케이스를 통과한 코드
+
 def solution(players, callings):
-
-    idx = {}
-    for i, name in enumerate(players):
-        idx[name] = i
-
-    print(idx)
-
-    for call in callings:
-        now = idx[call]
-        # 이 이름보다 하나 앞에 있는 사람의 value 값이랑 이 이름의 value 값이랑 바꾸면 되는데..
-        # idx[call], idx[now-1]
-
-    print(idx)
-    answer = idx
+    for i in range(len(callings)):
+        idx = players.index(callings[i])
+        players[idx], players[idx - 1] = players[idx - 1], players[idx]
+    answer = players
     return answer
 
 
 print(solution(players, callings))
+
+# 총평
+# 16개 테스트 케이스 중에 5개가 시간 초과로 실패했다..
+# 리스트 값을 계속 수정해서 오래 걸리는 것 이거나 이름 불리는 선수의 수가 많아져서 오래 걸리는 것 같음.
+# 이름 불린 수를 카운트해서 현재 위치에서 빼주는 방법도 생각했으나,
+# 숫자 값이 같은 경우 우위를 정하는 방법을 알 수 없었다.
