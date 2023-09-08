52704755f7b5b0cd4e14f9ef1ef70fad568320c9
조원주 <dnjswn9178@gmail.com>
2023-09-08 22:06:12 +0900

    solve 3020

--- Baekjoon/Gold/3020_bug.py
+++ Baekjoon/Gold/3020_bug.py
@@ -2,21 +2,46 @@ import sys
 sys.stdin = open('3020_bug.txt', 'r')
 
 N, H = map(int, sys.stdin.readline().split())
-
-prefix_num = [0] * H
-
-num_arr = [int(sys.stdin.readline()) for _ in range(N)]
+top = [0] * H 
+bottom = [0] * H
 
 for i in range(N):
-    for j in range(num_arr[i]):
-        if i % 2 == 0:
-            prefix_num[H - j - 1] += 1
-        else:
-            prefix_num[j] += 1
+    height = int(sys.stdin.readline())
+    if i % 2 == 0:
+        bottom[height - 1] += 1
+    else:
+        top[height - 1] += 1
 
+for i in range(H - 2, -1, -1):
+    top[i] += top[i + 1]
+    bottom[i] += bottom[i + 1]
+
+ans = N
 cnt = 0
 for i in range(H):
-    if prefix_num[i] == min(prefix_num):
+    if ans > top[i] + bottom[H - i - 1]:
+        ans = top[i] + bottom[H - i - 1]
+        cnt = 1
+    elif ans == top[i] + bottom[H - i - 1]:
         cnt += 1
 
-print(min(prefix_num), cnt)
+print(ans, cnt)
+
+# prefix_num = [0] * H
+
+# num_arr = [int(sys.stdin.readline()) for _ in range(N)]
+
+# for i in range(N):
+#     for j in range(num_arr[i]):
+#         if i % 2 == 0:
+#             prefix_num[H - j - 1] += 1
+#         else:
+#             prefix_num[j] += 1
+
+# ans = min(prefix_num)
+# cnt = 0
+# for i in range(H):
+#     if prefix_num[i] == ans:
+#         cnt += 1
+
+# print(ans, cnt)
