63c17884c0c2f447484e0554257f958713791009
조원주 <dnjswn9178@gmail.com>
2023-10-24 21:10:21 +0900

    solve

--- Baekjoon/Gold/2600_game.py
+++ Baekjoon/Gold/2600_game.py
@@ -1,9 +1,24 @@
 def recur(f, s):
-    pass
+    if dp[f][s] != -1:
+        return dp[f][s]
+    
+    for i in range(3):
+        if able[i] <= f and not recur(f - able[i], s):
+            dp[f][s] = 1
+            return dp[f][s]
+    for i in range(3):
+        if able[i] <= s and not recur(f, s - able[i]):
+            dp[f][s] = 1
+            return dp[f][s]
+    dp[f][s] = 0
+    return dp[f][s]
 
 
 able = list(map(int, input().split()))
-
+dp = [[-1] * 501 for _ in range(501)]
 for _ in range(5):
     first, second = map(int, input().split())
-    print(recur(first, second))
+    if recur(first, second):
+        print("A")
+    else:
+        print("B")
