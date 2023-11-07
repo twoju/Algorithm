1fa3752a91a95aeb5927ed6d4a6c89f63122da03
조원주 <dnjswn9178@gmail.com>
2023-11-07 23:20:47 +0900

    solve

--- Baekjoon/Gold/2668.py
+++ Baekjoon/Gold/2668.py
@@ -1,35 +1,33 @@
-def dfs(cur):
-    visited[cur] = True
-    if first == second:
-        ans.extend(sorted(first))
-        return
-    for i in number[cur]:
-        if not visited[i]:
-            first.append(cur)
-            second.append(i)
-            print(first, second)
-            dfs(i)
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
 
+def dfs(cur, arr):
+    visited[cur] = True
+    arr.add(cur)
+    
+    for i in number[cur]:
+        if i not in arr:
+            dfs(i, arr.copy())
+        else:
+            ans.extend(list(arr))
+            return
 
 n = int(input())
-
 number = [[] for _ in range(0, n + 1)]
 
 for i in range(n):
     a = int(input())
     number[a].append(i + 1)
 
-print(number)
-
 ans = []
 visited = [False] * (n + 1)
 
 for i in range(1, n + 1):
-    first = []
-    second = []
     if not visited[i]:
-        dfs(i)
+        dfs(i, set([]))
 
+ans.sort()
 print(len(ans))
 for i in ans:
     print(i)
