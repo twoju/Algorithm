da8dedfd5036356df7e2bf4753f786e75b658332
조원주 <dnjswn9178@hanmail.net>
2023-11-22 17:53:49 +0900

    not solve

---
+++ Baekjoon/Gold/12851.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+visited = [-1] * 100010
+q = deque()
+q.append(n)
+visited[n] = 0
+cnt = 0
+
+while q:
+    cur = q.popleft()
+    if cur == k:
+        cnt += 1
+        continue
+    for nxt in [cur + 1, cur -1, cur * 2]:
+        if nxt < 0 or nxt >= 100010:
+            continue
+        if visited[nxt] != -1:
+            continue
+        q.append(nxt)
+        visited[nxt] = visited[cur] + 1
+
+
+print(visited[k])
+print(cnt)
