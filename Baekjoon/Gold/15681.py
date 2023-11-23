ce29a7b147473d4271437c3f064b8204c06222eb
조원주 <dnjswn9178@hanmail.net>
2023-11-23 17:49:42 +0900

    solve

---
+++ Baekjoon/Gold/15681.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n, r, q = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+
+for _ in range(n - 1):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
