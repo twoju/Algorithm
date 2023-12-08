d0dc07f88961923f266fdbfe40b84ce62972e538
조원주 <dnjswn9178@gmail.com>
2023-12-09 01:07:54 +0900

    solve

--- Baekjoon/Gold/18116.py
+++ Baekjoon/Gold/18116.py
@@ -1,9 +1,8 @@
 import sys
 
 n = int(sys.stdin.readline())
-parent = [i for i in range(1000010)]
-rnk = [0 for i in range(1000010)]
-size = [1 for i in range(1000010)]
+parent = [i for i in range(1000001)]
+size = [1 for i in range(1000001)]
 
 
 def find(x):
@@ -14,22 +13,18 @@ def find(x):
 def union(a, b):
     a = find(a)
     b = find(b)
-    if rnk[a] < rnk[b]:
-        parent[a] = b
-        size[b] += size[a]
-    elif rnk[b] < rnk[a]:
+    if a < b:
         parent[b] = a
         size[a] += size[b]
-    else:
+    elif b < a:
         parent[a] = b
-        rnk[b] += 1
         size[b] += size[a]
 
 for _ in range(n):
-    bot = list(map(str, sys.stdin.readline().split()))
+    bot = list(sys.stdin.readline().split())
 
     if bot[0] == 'I':
         union(int(bot[1]), int(bot[2]))
-    if bot[0] == 'Q':
+    else:
         res = find(int(bot[1]))
         print(size[res])
--- Baekjoon/silver/11724.py
+++ Baekjoon/silver/11724.py
@@ -2,30 +2,54 @@ import sys
 sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
-def dfs(cur):
-    visited[cur] = True
-    for i in graph[cur]:
-        if not visited[i]:
-            dfs(i)
-
 n, m = map(int, input().split())
-graph = [[] for _ in range(n + 1)]
-visited = [False] * (n + 1)
+parent = [i for i in range(n + 1)]
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    parent[b] = a
 
 for _ in range(m):
     u, v = map(int, input().split())
-    graph[u].append(v)
-    graph[v].append(u)
+    if find(u) != find(v):
+        union(u, v)
 
-ans = 0
 for i in range(1, n + 1):
-    if not visited[i]:
-        if not graph[i]:
-            ans += 1
-            visited[i] = True
-        else:
-            dfs(i)
-            ans += 1
+    find(i)
 
-print(ans)
+ans = set(filter(lambda x:x > 0, parent))
+print(len(ans))
+# def dfs(cur):
+#     visited[cur] = True
+#     for i in graph[cur]:
+#         if not visited[i]:
+#             dfs(i)
+
+# n, m = map(int, input().split())
+# graph = [[] for _ in range(n + 1)]
+# visited = [False] * (n + 1)
+
+# for _ in range(m):
+#     u, v = map(int, input().split())
+#     graph[u].append(v)
+#     graph[v].append(u)
+
+# ans = 0
+# for i in range(1, n + 1):
+#     if not visited[i]:
+#         if not graph[i]:
+#             ans += 1
+#             visited[i] = True
+#         else:
+#             dfs(i)
+#             ans += 1
+
+# print(ans)
 
