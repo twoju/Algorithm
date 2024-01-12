1e912316d750cdc4bccd14a24a2867e0c7774a12
twoju <122516213+twoju@users.noreply.github.com>
2024-01-13 01:27:42 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1092 ms, Memory: 105992 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -0,0 +1,17 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+prefix_sum = [[0] * (N + 1) for _ in range(N + 1)]
+
+input = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+for i in range(1, N + 1):
+    for j in range(1, N + 1):
+        prefix_sum[i][j] = prefix_sum[i - 1][j] + prefix_sum[i][j - 1] - prefix_sum[i - 1][j - 1] + input[i - 1][j - 1]
+
+for _ in range(M):
+    ans = 0
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+    ans += prefix_sum[x2][y2] - prefix_sum[x2][y1 - 1] - prefix_sum[x1 - 1][y2] + prefix_sum[x1 - 1][y1 - 1]
+    print(ans)
