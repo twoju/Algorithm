03c39edd1bee26dd7568e5b0aadc1f7f2cabcdc4
twoju <122516213+twoju@users.noreply.github.com>
2024-02-07 13:40:24 +0900

    [Gold V] Title: 암호 만들기, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,31 +1,23 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 6)
+
+def recur(cur, ans):
+    if cur == c:
+        if len(ans) == l:
+            vo, co = 0, 0
+            for a in ans:
+                if a in ['a', 'e', 'i', 'o', 'u']:
+                    vo += 1
+                else:
+                    co += 1
+                if vo >= 1 and co >= 2:
+                    print(ans)
+                    return
+        return
+    recur(cur + 1, ans + arr[cur])
+    recur(cur + 1, ans)
 
 l, c = map(int, input().split())
-alpha = sorted(list(map(str, input().split())))
-
-vowels = ['a', 'e', 'i', 'o', 'u']
-
-def recur(cur, prev):
-    if cur == l:
-        vo, co = 0, 0
-        for w in res:
-            if w in vowels:
-                vo += 1
-            else: co += 1
-        if vo > 0 and co > 1:
-            for r in res:
-                print(r, end='')
-            print('')
-        return
-    for i in range(prev, c):
-        if visited[i]:
-            continue
-        res[cur] = alpha[i]
-        visited[i] = True
-        recur(cur + 1, i + 1)
-        visited[i] = False
-
-visited = [False] * c
-res = [0] * l
-recur(0, 0)
+arr = sorted(list(map(str, input().split())))
+recur(0, '')
