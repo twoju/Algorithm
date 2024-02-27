eb2ec774e9bb76a0770d6f87f9579d61528dac01
조원주 <dnjswn9178@gmail.com>
2024-02-27 19:39:18 +0900

    update

--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -1,16 +1,36 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(4000)
 
-n = int(input().strip())
-lines = [list(map(int, input().split())) for _ in range(n)]
-lines.sort(key=lambda x:x[0])
-dp = [1] * n
+def recur(cur):
+    if cur > n:
+        return 0
+    if dp[cur] != -100000:
+        return dp[cur]
+    ret = 1e9
+    for i in range(cur, n + 1):
+        ret = min(ret, pref_sum[i] - pref_sum[cur - 1] - recur(i + 1))
+    dp[cur] = ret
+    return dp[cur]
 
-for i in range(n):
-    for j in range(i):
-        if lines[i][1] > lines[j][1]:
-            dp[i] = max(dp[i], dp[j] + 1)
-print(n - max(dp))
+
+n = int(input())
+for _ in range(3):
+    arr = list(map(int, input().split()))
+    arr.reverse()
+    pref_sum = [0] * (n + 1)
+    for i in range(1, n + 1):
+        pref_sum[i] = pref_sum[i - 1] + arr[i - 1]
+
+    dp = [-100000] * 3030
+    ans = recur(1)
+
+    if ans < 0:
+        print('A')
+    elif ans > 0:
+        print('B')
+    else:
+        print('D')
 
 # -----
 # // recur(인자, 인자) => 인자만큼 dp를 만들면 된다
