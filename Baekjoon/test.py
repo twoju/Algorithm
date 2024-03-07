75af5c56b903ad88716e3b68729b368f4db55f22
조원주 <dnjswn9178@gmail.com>
2024-03-07 19:02:43 +0900

    update

--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -1,17 +1,35 @@
 import sys
 input = sys.stdin.readline
 
+def dfs(x, y):
+    if y == c - 1:
+        return True
+    for nxt in [-1, 0, 1]:
+        nx, ny = x + nxt, y + 1
+        if nx < 0 or nx >= r or ny < 0 or ny >= c:
+            continue
+        if visited[nx][ny]:
+            continue
+        if board[nx][ny] == 1:
+            continue
+        visited[nx][ny] = True
+        if dfs(nx, ny):
+            return True
+    return False
+
+
+r, c = map(int, input().split())
+board = []
+for _ in range(r):
+    arr = list(map(lambda x: 0 if x == '.' else 1, input().strip()))
+    board.append(arr)
 ans = 0
-x, y = 0, 0
-for i in range(1, 10):
-    arr = list(map(int, input().split()))
-    for j in range(9):
-        if arr[j] >= ans:
-            ans = arr[j]
-            x, y = i, j + 1
+visited = [[False] * c for _ in range(r)]
+for i in range(r):
+    if dfs(i, 0):
+        ans += 1
 
 print(ans)
-print(x, y)
 
 # -----
 # // recur(인자, 인자) => 인자만큼 dp를 만들면 된다
