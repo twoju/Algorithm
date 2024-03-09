f37d2144506d58ba932d919e43bc66c0f0efed96
조원주 <dnjswn9178@gmail.com>
2024-03-10 01:21:16 +0900

    update

--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -1,10 +1,23 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-word = input().strip()
-ans = 0
-for i in range(len(str(word))):
-    if word[i] in ['a', 'e', 'i', 'o', 'u']:
-        ans += 1
+n, f, s = map(int, input().split())
+crave = [[] for _ in range(n + 1)]
+for _ in range(n - 1):
+    u, v, l = map(int, input().split())
+    crave[u].append((v, l))
+    crave[v].append((u, l))
 
-print(ans)
+visited = [False] * (n + 1)
+q = deque([(f, 0, 0)])
+while q:
+    cur, total, tmp = q.pop()
+    visited[cur] = True
+    if cur == s:
+        print(total - tmp)
+        exit(0)
+    for nxt, cnt in crave[cur]:
+        if visited[nxt]:
+            continue
+        q.appendleft((nxt, total + cnt, max(tmp, cnt)))
