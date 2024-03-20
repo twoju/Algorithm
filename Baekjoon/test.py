230137598ade7217fa5598cb627d8ace7aae75d5
조원주 <dnjswn9178@gmail.com>
2024-03-21 00:14:22 +0900

    update

--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -1,29 +1,37 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
 
-def dfs(x, y):
-    visited[x][y] = True
-    for i in range(8):
-        dx, dy = x + dxy[i][0], y + dxy[i][1]
-        if dx < 0 or dy < 0 or dx >= h or dy >= w:
-            continue
-        if visited[dx][dy]:
-            continue
-        if board[dx][dy] == 1:
-            dfs(dx, dy)
+def bfs():
+    while q:
+        x, y = q.popleft()
+        for nxt in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+            dx, dy = x + nxt[0], y + nxt[1]
+            if dx < 0 or dy < 0 or dx >= n or dy >= m:
+                continue
+            if board[dx][dy] == 0:
+                continue
+            if dist[dx][dy] == -1:
+                q.append((dx, dy))
+                dist[dx][dy] = dist[x][y] + 1
 
-while True:
-    w, h = map(int, input().split())
-    if w == 0 and h == 0:
-        break
-    board = [list(map(int, input().split())) for _ in range(h)]
-    visited = [[False] * w for _ in range(h)]
-    ans = 0
-    dxy = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-    for x in range(h):
-        for y in range(w):
-            if board[x][y] == 1 and not visited[x][y]:
-                dfs(x, y)
-                ans += 1
+n, m = map(int, input().split())
+board = []
+dist = [[-1] * m for _ in range(n)]
+q = deque()
 
-    print(ans)
+for i in range(n):
+    arr = list(map(int, input().split()))
+    for j in range(m):
+        if arr[j] == 0:
+            dist[i][j] = 0
+        if arr[j] == 2:
+            q.append((i, j))
+            dist[i][j] = 0
+    board.append(arr)
+
+
+bfs()
+
+for arr in dist:
+    print(*arr)
