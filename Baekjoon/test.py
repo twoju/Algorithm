71cedacf6aff1caccc155b450117929bb483727e
조원주 <dnjswn9178@gmail.com>
2024-03-21 21:21:47 +0900

    update

--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -2,36 +2,33 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-def bfs():
-    while q:
-        x, y = q.popleft()
-        for nxt in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
-            dx, dy = x + nxt[0], y + nxt[1]
-            if dx < 0 or dy < 0 or dx >= n or dy >= m:
-                continue
-            if board[dx][dy] == 0:
-                continue
-            if dist[dx][dy] == -1:
-                q.append((dx, dy))
-                dist[dx][dy] = dist[x][y] + 1
 
 n, m = map(int, input().split())
-board = []
-dist = [[-1] * m for _ in range(n)]
+ladder = [0] * 101
+
+for _ in range(n + m):
+    x, y = map(int, input().split())
+    ladder[x] = y
+
 q = deque()
+q.append((1, 0))
+visited = [False] * 101
+ans = 1e9
+visited[1] = True
+while q:
+    cur, cnt = q.popleft()
+    if cur == 100:
+        ans = min(ans, cnt)
+        break
+    for i in range(1, 7):
+        if cur + i > 100:
+            continue
+        if visited[cur + i]:
+            continue
+        if ladder[cur + i] != 0:
+            q.append((ladder[cur + i], cnt + 1))
+        else:
+            q.append((cur + i, cnt + 1))
+        visited[cur] = True
 
-for i in range(n):
-    arr = list(map(int, input().split()))
-    for j in range(m):
-        if arr[j] == 0:
-            dist[i][j] = 0
-        if arr[j] == 2:
-            q.append((i, j))
-            dist[i][j] = 0
-    board.append(arr)
-
-
-bfs()
-
-for arr in dist:
-    print(*arr)
+print(ans)
