9e4064bf968a51c1a63b5019e82772eff6aa2254
조원주 <dnjswn9178@gmail.com>
2024-03-26 17:01:31 +0900

    update

--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -1,22 +1,16 @@
 import sys
 input = sys.stdin.readline
 
-def check(cur):
-    global cnt
-    if cur == n:
-        cnt += 1
-        return
-    if cur > n:
-        return
-    for nxt in range(1, 4):
-        if cur + nxt > n:
-            continue
-        check(cur + nxt)
+n, k = map(int, input().split())
 
-
-t = int(input().strip())
-for _ in range(t):
-    n = int(input().strip())
-    cnt = 0
-    check(0)
-    print(cnt)
+if n == k:
+    print('Impossible')
+elif n == k + 1:
+    for i in range(1, n + 1):
+        print(i, end=' ')
+else:
+    print(k + 2, end=' ')
+    for i in range(2, k + 2):
+        print(i, end=' ')
+    for i in range(k + 2, n + 1):
+        print(i + 1 if i < n else 1, end=' ')
