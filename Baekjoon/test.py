972b9ef6867f4b16be1ffe5458454cd9ef1a822a
조원주 <dnjswn9178@gmail.com>
2024-03-29 17:52:31 +0900

    update

--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -1,16 +1,31 @@
 import sys
 input = sys.stdin.readline
+from collections import defaultdict
+sys.setrecursionlimit(10 ** 6)
 
-n, k = map(int, input().split())
+def recur(cur, prev):
+    for nxt in graph[cur]:
+        if nxt == prev:
+            continue
+        if dp[nxt][0] or dp[nxt][1]:
+            continue
+        recur(nxt, cur)
+        dp[cur][1] += dp[nxt][0]
+        dp[cur][0] += max(dp[nxt][0], dp[nxt][1] + citizen[nxt])
 
-if n == k:
-    print('Impossible')
-elif n == k + 1:
-    for i in range(1, n + 1):
-        print(i, end=' ')
-else:
-    print(k + 2, end=' ')
-    for i in range(2, k + 2):
-        print(i, end=' ')
-    for i in range(k + 2, n + 1):
-        print(i + 1 if i < n else 1, end=' ')
+n = int(input().strip())
+citizen = [0] + list(map(int, input().split()))
+graph = defaultdict(list)
+for _ in range(n - 1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+# 인접하지 않은 마을들 사이의 인구 합이 최대로
+# 우수마을이 아닌 곳은 하나의 마을은 꼭 연결 되어야함
+    
+dp = [[0] * 2 for _ in range(n + 1)]
+dp[1][1] = citizen[1]
+
+recur(1, 1)
+print(max(dp[1]))
