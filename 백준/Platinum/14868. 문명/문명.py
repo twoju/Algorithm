5eb6a754587300f3a4c90096c9c5c0af25ab14a9
twoju <122516213+twoju@users.noreply.github.com>
2024-04-03 19:01:50 +0900

    [Platinum IV] Title: 문명, Time: 3972 ms, Memory: 700744 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Platinum/14868. 문명/문명.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+
+n, k = map(int, input().split())
+parent = [i for i in range(k + 1)]
+visited = [[0 for _ in range(2001)] for _ in range(2001)]
+dxy = [(0, -1), (-1, 0), (0, 1), (1, 0)]
+
+q = deque()
+for i in range(1, k + 1):
+    x, y = map(int, input().split())
+    q.append((x, y, i, 0))
+
+def bfs():
+    cnt = 0
+    while q:
+        x, y, c, t = q.popleft()
+        if visited[x][y]:
+            continue
+        visited[x][y] = c
+        for i in range(4):
+            dx, dy = x + dxy[i][0], y + dxy[i][1]
+            if dx < 1 or dy < 1 or dx > n or dy > n:
+                continue
+            cur = visited[dx][dy]
+            if cur:
+                if find(c) != find(cur):
+                    parent[parent[cur]] = parent[c]
+                    cnt += 1
+                    if cnt == k - 1:
+                        return t
+            else:
+                q.append((dx, dy, c, t + 1))
+
+print(bfs())
