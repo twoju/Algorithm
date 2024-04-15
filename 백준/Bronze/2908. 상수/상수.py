54997835536792f7627cd572e105d075fcccd898
twoju <122516213+twoju@users.noreply.github.com>
2024-04-15 13:36:32 +0900

    [Bronze II] Title: 상수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+a, b = map(str, input().split())
+new_a = a[::-1]
+new_b = b[::-1]
+
+if int(new_a) > int(new_b):
+    print(new_a)
+else:
+    print(new_b)
