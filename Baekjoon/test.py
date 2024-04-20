8c42fe8c9f85c782c2ca5470061a2c664ee7333f
조원주 <dnjswn9178@gmail.com>
2024-04-21 08:52:22 +0900

    update

--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -1,20 +1,36 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input().strip())
-point = []
-x = []
-y = []
-for _ in range(n):
-    a, b = map(int, input().split())
-    point.append((a, b))
-    x.append(a)
-    y.append(b)
+a = [''] + list(input().strip())
+b = [''] + list(input().strip())
 
-x.sort()
-y.sort()
+len_a = len(a)
+len_b = len(b)
 
-ans = 0
-for a, b in point:
-    ans += abs(a - x[(n - 1) // 2]) + abs(b - y[(n - 1) // 2])
-print(ans)
+dp = [[0] * len_b for _ in range(len_a)]
+
+for i in range(1, len_a):
+    for j in range(1, len_b):
+        if a[i] == b[j]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+n = dp[len_a - 1][len_b - 1]
+print(n)
+
+lcs = ''
+s, e = len_a - 1, len_b - 1
+while s > 0 and e > 0:
+    if a[s] == b[e]:
+        lcs = a[s] + lcs
+        if len(lcs) == n:
+            print(lcs)
+            break
+        s -= 1
+        e -= 1
+    else:
+        if dp[s - 1][e] > dp[s][e - 1]:
+            s -= 1
+        else:
+            e -= 1
