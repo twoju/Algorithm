5aa056a598754d57d238d652c89a97fb8647e28f
조원주 <dnjswn9178@gmail.com>
2024-04-30 22:22:37 +0900

    update

--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -1,36 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-a = [''] + list(input().strip())
-b = [''] + list(input().strip())
+def recur(cur, prev):
+    if cur == m:
+        print(*selected)
+        return
+    for idx in range(prev, n):
+        if arr[idx] < arr[prev]:
+            continue
+        nxt = arr[idx]
+        selected[cur] = nxt
+        recur(cur + 1, idx)
 
-len_a = len(a)
-len_b = len(b)
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
 
-dp = [[0] * len_b for _ in range(len_a)]
-
-for i in range(1, len_a):
-    for j in range(1, len_b):
-        if a[i] == b[j]:
-            dp[i][j] = dp[i - 1][j - 1] + 1
-        else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-
-n = dp[len_a - 1][len_b - 1]
-print(n)
-
-lcs = ''
-s, e = len_a - 1, len_b - 1
-while s > 0 and e > 0:
-    if a[s] == b[e]:
-        lcs = a[s] + lcs
-        if len(lcs) == n:
-            print(lcs)
-            break
-        s -= 1
-        e -= 1
-    else:
-        if dp[s - 1][e] > dp[s][e - 1]:
-            s -= 1
-        else:
-            e -= 1
+selected = [0] * m
+recur(0, 0)
