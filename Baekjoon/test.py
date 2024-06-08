7e3cb476aa750dad3a8bdc33ef0ffbf871b21f51
조원주 <dnjswn9178@gmail.com>
2024-06-08 14:03:27 +0900

    update

--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -1,15 +1,25 @@
 import sys
 input = sys.stdin.readline
 
-t = int(input().strip())
-pref_sum = [0] * 1000010
-pref_sum[0] = 1
-for i in range(1, 1000010):
-    pref_sum[i] = pref_sum[i - 1] + str(i).count('0')
+n = int(input().strip())
+snowball = sorted(list(map(int, input().split())))
+ans = 2000000000
 
-for _ in range(t):
-    n, m = map(int, input().split())
-    if n == 0:
-        print(pref_sum[m])
-    else:
-        print(pref_sum[m] - pref_sum[n - 1])
+for i in range(n - 3):
+    for j in range(i + 3, n):
+        first = snowball[i] + snowball[j]
+        s, e = i + 1, j - 1
+        while s < e:
+            second = snowball[s] + snowball[e]
+            dist = abs(first - second)
+            if ans > dist:
+                ans = dist
+            if first > second:
+                s += 1
+            else:
+                e -= 1
+        if ans == 0:
+            print(0)
+            sys.exit(0)
+
+print(ans)
