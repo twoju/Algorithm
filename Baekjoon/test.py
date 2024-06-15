a54a3ee4b5f09d520626d78dc8fc03b365f9882d
조원주 <dnjswn9178@gmail.com>
2024-06-15 18:36:41 +0900

    update

--- Baekjoon/test.py
+++ Baekjoon/test.py
@@ -17,25 +17,29 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input().strip())
-nrr = sorted(list(map(int, input().split())))
-m = int(input().strip())
-mrr = list(map(int, input().split()))
+t = int(input().strip())
+for _ in range(t):
+    n = int(input().strip())
+    whole = sorted(list(map(int, input().split())))
+    m = int(input().strip())
+    see = list(map(int, input().split()))
 
-for a in mrr:
-    flag = 0
-    s, e = 0, n - 1
-    while s <= e:
-        mid = (s + e) // 2
-        if a == nrr[mid]:
-            flag = 1
-            break
-        if a < nrr[mid]:
-            e = mid - 1
+    for i in range(m):
+        if see[i] > whole[-1] or see[i] < whole[0]:
+            print(0)
+            continue
+        s, e = 0, n - 1
+        flag = 0
+        while s <= e:
+            mid = (s + e) // 2
+            if whole[mid] == see[i]:
+                flag = 1
+                break
+            if whole[mid] < see[i]:
+                s = mid + 1
+            else:
+                e = mid - 1
+        if flag:
+            print(1)
         else:
-            s = mid + 1
-
-    if flag:
-        print(1)
-    else:
-        print(0)
+            print(0)
